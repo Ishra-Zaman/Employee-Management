1249adae494acc7f30a849ce8f72772a2ac170de
“Ishra <ishraz112@gmail.com>
2024-03-31 14:15:57 -0400

    Migrate PGDatabase to Sequelize. Migrate Designation Module.

--- API/utils/utils.js
+++ API/utils/utils.js
@@ -2,4 +2,8 @@ function isErrorAForeignKeyViolation(error) {
     return error.includes("foreign") && error.includes("key") && error.includes("violates");
 }
 
-module.exports = {isErrorAForeignKeyViolation}
+function displayValidationErrorMessages(errors) {
+    return errors?.map((e) => `Field: ${e.path} - Msg: ${e.message}`)
+}
+
+module.exports = {isErrorAForeignKeyViolation, displayValidationErrorMessages}
